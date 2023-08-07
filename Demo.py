f91f461e411877d7abdc4b6af3b3c2bdebdb5189
Beeeee <beeeee2@outlook.com>
2023-08-07 09:39:46 +0800

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,33 +1,13 @@
 import random
-
 while True:
-    player = input("输入:(1:剪刀 2:石头 3:布 0:退出)")
-
-    computer = str(random.randint(1, 3))
-    computer_ = ""
-    if computer == "1":
-        computer_ = "剪刀"
-    elif computer == "2":
-        computer_ = "石头"
-    elif computer == "3":
-        computer_ = "布"
+    i = input("hi!")
+    if i == "hi!" :
+        print("qwq")
+    elif i == "qwq":
+        print(":))))))")
+    elif i == "Random":
+        i1 = input("i1:")
+        i2 = input("i2:")
+        '''print(random.randint(i1,i2))'''
     else:
-        computer_ = "error"
-        break
-    print("电脑:" + computer_)
-    if player == computer:
-        print("平局")
-    elif player == "1" and computer == "2" or player == "2" and computer == "3" or player == "3" and computer == "1":
-        print("输了:(")
-    elif player == "1" and computer == "3" or player == "2" and computer == "1" or player == "3" and computer == "2":
-        print("赢了:)")
-    elif player == "0":
-        break
-    else:
-        print("你在输入什么？？？？？")
-
-    i = 0
-    while i < 10000:
-        print("-", end="")
-        i += 1
-    print("")
+        print("awa")
